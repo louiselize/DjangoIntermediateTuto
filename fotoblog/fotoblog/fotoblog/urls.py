1b15dbb912203c320186b1a969cd406b3720108f
Louise Lizé <louise.lize@icloud.com>
2024-05-30 19:56:44 +0200

    ♻️ use generic views made by django

--- fotoblog/fotoblog/fotoblog/urls.py
+++ fotoblog/fotoblog/fotoblog/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path
 import authentication.views
 import blog.views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', authentication.views.login_page, name='login'),
+    path('', LoginView.as_view(
+            template_name='authentication/login.html',
+            redirect_authenticated_user=True),
+        name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', blog.views.home, name='home'),
 ]
